b7e68f12a65ae83149a7d7a0c7fbca5d4becf724
joseph emmanuel kayode (iemarjay) <emmanueljoseph@fitted.ng>
2024-08-26 17:16:16 +0100

    Update creator.ts

--- src/actions/creator.ts
+++ src/actions/creator.ts
@@ -1,13 +1,14 @@
 "use server";
 import prisma from "@/helpers/prisma";
+import { PrismaPromise } from "@prisma/client";
 
 export async function findOrCreateCreator(address: string) {
   const creator = await prisma.creator.findFirst({
     where: { address },
   });
-  if (creator) return creator;
+  if (creator) return creator as unknown as PrismaPromise<Creator>;
 
   return prisma.creator.create({
     data: { address },
-  });
+  }) as unknown as PrismaPromise<Creator>;
 }
